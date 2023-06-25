fbb560689d00f982d4af11f5efcf07ad0437c070
shiangah francis <shiangahfrancis@outlook.com>
2023-06-25 13:17:31 +0000

    added interfaces

---
+++ Interfaces/IGenericRepository.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Linq.Expressions;
+
+namespace HotelApi.Interfaces
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<IList<T>> GetAll(
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy,
+            Expression<Func<T, bool>> expression = null,
+            List<string> includes = null
+        );
+
+        Task<IList<T>> GetAll(
+            Expression<Func<T, bool>> expression,
+            List<string> includes = null
+            );
+
+        Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null);
+
+        void Insert(T entity);
+
+        void InsertRange(IEnumerable<T> entities);
+
+        void Update(T entity);
+
+        void DeleteRange(IEnumerable<T> entities);
+
+        void Delete(int id);
+    }
+}
---
+++ Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using HotelApi.Data;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace HotelApi.Interfaces
+{
+    public interface IUnitOfWork: IDisposable
+    {
+        public IGenericRepository<Country> CountryRepository { get; }
+
+        public IGenericRepository<Hotel> HotelRepository { get; }
+
+        public void SaveChanges();
+    }
+}
