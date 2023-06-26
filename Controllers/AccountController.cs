2328bea0d2a75d5a9a093c8fa24dfdc6e2cd2461
shiangah francis <shiangahfrancis@outlook.com>
2023-06-26 10:31:39 +0000

    added account controllers

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,96 @@
+﻿using AutoMapper;
+using HotelApi.Data;
+using HotelApi.DTOS.WriteDtos;
+using HotelApi.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace HotelApi.Controllers
+{
+    [Route("api/[controller")]
+    [ApiController]
+    public class AccountController :ControllerBase
+    {
+        private readonly UserManager<ApiUser> _userManager;
+        private readonly SignInManager<ApiUser> _signInManager;
+        private readonly IMapper _mapper;
+        private readonly ILogger<AccountController> _logger;
+        private readonly IAuthManager _authManager;
+        public AccountController(
+            ILogger<AccountController> logger,
+            IMapper mapper,
+            SignInManager<ApiUser> signInManager,
+            UserManager<ApiUser> userManager,
+            IAuthManager authManager)
+        {
+            _logger = logger;
+            _mapper = mapper;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _authManager = authManager;
+        }
+
+        [HttpPost("Login", Name = "Register")]
+        public async Task<IActionResult> Register([FromBody] UserWriteDto userWriteDto)
+        {
+            var user = _mapper.Map<ApiUser>(userWriteDto);
+            if (ModelState.IsValid is not true)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var result = await _userManager.CreateAsync(user, userWriteDto.Password);
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRolesAsync(user, userWriteDto.Roles);
+                    return StatusCode(201, "User Created Successfully");
+
+                }
+                else
+                {
+                    return BadRequest(result.Errors);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, $"something went wrong in {nameof(Register)}");
+                return StatusCode(500, "An error occured. Please try again later");
+            }
+        }
+
+        [HttpPost("register", Name = "Login")]
+        public async Task<IActionResult> Login([FromBody] LoginWriteDto loginInfo)
+        {
+
+
+            if (ModelState.IsValid is not true)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                var isUserValidated = await _authManager.AuthenticateUserAsync(loginInfo);
+                if (isUserValidated)
+                {
+                    var token = await _authManager.GenerateTokenAsync();
+                    return Accepted(new {Token = token});
+
+                }
+                else
+                {
+                    return Unauthorized();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, $"something went wrong in {nameof(Register)}");
+                return StatusCode(500, "An error occured. Please try again later");
+            }
+        }
+    }
+}
---
+++ Controllers/HotelController.cs
@@ -0,0 +1,65 @@
+﻿using AutoMapper;
+using HotelApi.DTOS.ReadDtos;
+using HotelApi.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotelApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger _logger;
+        private readonly IMapper _mapper;
+
+        public HotelController(IUnitOfWork unitOfWork, ILogger logger, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            try
+            {
+                var hotels = await _unitOfWork.HotelRepository.GetAll();
+                if(hotels.Count > 0)
+                {
+                    var mappedHotels = _mapper.Map<HotelReadDto>(hotels);
+                    return Ok(mappedHotels);
+                }
+                return StatusCode(200, "No Hotels Found");
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, $"Something went wrong in {nameof(HotelController.GetAll)}");
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [Authorize]
+        [HttpGet("by/id/{id}:int")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                var hotel = await _unitOfWork.HotelRepository.Get(h => h.Id == id);
+                if (hotel is not null) {
+                    var mappedHotel = _mapper.Map<HotelReadDto>(hotel);
+                    return Ok(mappedHotel);
+                 };
+                return StatusCode(200, "No item found for the specified Id");
+            }
+            catch (Exception ex)
+            {
+                _logger.Log(LogLevel.Error, ex, $"Something went wrong in {nameof(HotelController.GetById)}");
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
